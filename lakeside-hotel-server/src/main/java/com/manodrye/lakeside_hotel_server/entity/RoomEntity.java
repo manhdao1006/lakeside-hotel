20c9fe54343f3ba66097986311b558e3fd8368df
Đào Đức Mạnh <manhdao1006@gmail.com>
2024-06-12 21:09:51 +0700

    create-entity

---
+++ lakeside-hotel-server/src/main/java/com/manodrye/lakeside_hotel_server/entity/RoomEntity.java
@@ -0,0 +1,63 @@
+package com.manodrye.lakeside_hotel_server.entity;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class RoomEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "type")
+    private String roomType;
+
+    @Column(name = "price")
+    private BigDecimal roomPrice;
+
+    @Column(name = "is_booked")
+    private boolean isBooked = false;
+
+    @Column(name = "photo")
+    @Lob
+    private Blob photo;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoomEntity> bookings;
+
+    public RoomEntity(){
+        this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoomEntity bookingEntity){
+        if(bookings == null){
+            bookings = new ArrayList<>();
+        }
+        bookings.add(bookingEntity);
+        bookingEntity.setRoom(this);
+        isBooked = true;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        bookingEntity.setBookingConfirmationCode(bookingCode);
+    }
+}
