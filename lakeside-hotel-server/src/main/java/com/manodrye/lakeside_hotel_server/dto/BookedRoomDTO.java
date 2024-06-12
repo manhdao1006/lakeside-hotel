9a24279a123de02e29be5590ace2bb384103b382
Đào Đức Mạnh <manhdao1006@gmail.com>
2024-06-12 21:23:32 +0700

    create-dto

--- lakeside-hotel-server/src/main/java/com/manodrye/lakeside_hotel_server/dto/BookedRoomDTO.java
+++ lakeside-hotel-server/src/main/java/com/manodrye/lakeside_hotel_server/dto/BookedRoomDTO.java
@@ -1,5 +1,31 @@
 package com.manodrye.lakeside_hotel_server.dto;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoomDTO {
-    
+    private Long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfirmationCode;
+    private RoomDTO room;
+
+    public BookedRoomDTO(Long id, LocalDate checkInDate, LocalDate checkOutDate,
+            String bookingConfirmationCode) {
+        this.id = id;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }    
 }
--- lakeside-hotel-server/src/main/java/com/manodrye/lakeside_hotel_server/dto/RoomDTO.java
+++ lakeside-hotel-server/src/main/java/com/manodrye/lakeside_hotel_server/dto/RoomDTO.java
@@ -1,5 +1,36 @@
 package com.manodrye.lakeside_hotel_server.dto;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class RoomDTO {
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked;
+    private String photo;
+    private List<BookedRoomDTO> bookings;
     
+    public RoomDTO(Long id, String roomType, BigDecimal roomPrice) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+    }
+
+    public RoomDTO(Long id, String roomType, BigDecimal roomPrice, boolean isBooked, byte[] photoBytes,
+            List<BookedRoomDTO> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+        this.photo = photoBytes != null ? Base64.encodeBase64String(photoBytes) : null;
+        this.bookings = bookings;
+    }    
 }
