5499ff0b77bcac1e2ab16ada1de54778da56b4cb
Đào Đức Mạnh <manhdao1006@gmail.com>
2024-06-23 19:20:38 +0700

    browse-all-rooms

--- lakeside-hotel-client/src/App.jsx
+++ lakeside-hotel-client/src/App.jsx
@@ -7,6 +7,7 @@ import EditRoom from './components/room/EditRoom';
 import AddRoom from './components/room/AddRoom';
 import NavBar from './components/layout/NavBar';
 import Footer from './components/layout/Footer';
+import RoomListing from './components/room/RoomListing';
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
           <Route path='/add-room' element={ <AddRoom /> } />
           <Route path='/edit-room/:roomId' element={ <EditRoom /> } />
           <Route path='/existing-rooms' element={ <ExistingRooms /> } />
+          <Route path='/browse-all-rooms' element={ <RoomListing /> } />
         </Routes>
       </Router>
       
